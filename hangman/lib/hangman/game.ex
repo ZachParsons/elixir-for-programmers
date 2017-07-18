e3f0ec5faa33b01762f61b28e558844595d237f0
thebrianemory <the@brianemory.com>
2017-07-18 11:40:47 -0700

    Add tests for occurence of a letter not used
    Added tests for ensuring that the first occurence of a letter will not
    return :already_used and for the second occurence of a letter will
    return :already_used.

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -4,6 +4,7 @@ defmodule Hangman.Game do
     turns_left: 7,
     game_state: :initializing,
     letters:    [],
+    used:       MapSet.new(),
   )
 
   def new_game() do
@@ -16,7 +17,20 @@ defmodule Hangman.Game do
     { game, tally(game) }
   end
 
-  def tally(game) do
+  def make_move(game, guess) do
+    game = accept_move(game, guess, MapSet.member?(game.used, guess))
+    { game, tally(game) }
+  end
+
+  def accept_move(game, _guess, _already_guessed = true) do
+    Map.put(game, :game_state, :already_used)
+  end
+
+  def accept_move(game, guess, _alread_guessed) do
+    Map.put(game, :used, MapSet.put(game.used, guess))
+  end
+
+  def tally(_game) do
     123
   end
 end
--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -4,7 +4,6 @@ defmodule GameTest do
   alias Hangman.Game
 
   test "new_game returns structure" do
-
     game = Game.new_game()
 
     assert game.turns_left == 7
@@ -17,5 +16,22 @@ defmodule GameTest do
       game = Game.new_game() |> Map.put(:game_state, state)
       assert { ^game, _ } = Game.make_move(game, "X")
     end
+  end
+
+  test "first occurence of letter is not already used" do
+    game = Game.new_game()
+    { game, _tally } = Game.make_move(game, "x")
+
+    assert game.game_state != :already_used
+  end
+
+  test "second occurence of letter is not already used" do
+    game = Game.new_game()
+
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state == :already_used
   end
 end
