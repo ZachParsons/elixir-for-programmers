a2ac06ead22ea7f976fd8e71d017ea9a82933124
thebrianemory <the@brianemory.com>
2017-07-26 08:58:19 -0700

    Project Structure section

--- gallows/lib/gallows_web/views/page_view.ex
+++ gallows/lib/gallows_web/views/page_view.ex
@@ -1,3 +1,10 @@
 defmodule GallowsWeb.PageView do
   use GallowsWeb, :view
+
+  def plural_of(word, 0), do: "No #{word}"
+  def plural_of(word, 1), do: "1 #{word}"
+  def plural_of(word, amount) when amount > 0, do: "#{amount} #{word}" <> "s"
+  def plural_of(word, amount) when amount < 0 do
+    { :safe, "<span style='color: red'>#{amount} #{word}s</span>" }
+  end
 end
