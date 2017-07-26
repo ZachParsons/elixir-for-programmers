2fdf3569e9486cfc8206c695254a1bff62ac3240
thebrianemory <the@brianemory.com>
2017-07-26 09:22:13 -0700

    Assigns and @variables section

--- gallows/lib/gallows_web/controllers/page_controller.ex
+++ gallows/lib/gallows_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule GallowsWeb.PageController do
   use GallowsWeb, :controller
 
   def index(conn, _params) do
+    my_assigns = %{ produce: "apple", count: 3 }
     render conn, "index.html"
   end
 end
